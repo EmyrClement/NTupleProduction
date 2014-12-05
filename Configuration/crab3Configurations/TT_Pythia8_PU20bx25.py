f4cd4274aea8358d5af00a981e42af8437ab015f
Emyr Clement <emyr.clement@bristol.ac.uk>
2014-12-05 14:46:23 +0000

    Add crab3 configurations for PHYS14 samples.

--- Configuration/crab3Configurations/TT_Pythia8_PU20bx25.py
+++ Configuration/crab3Configurations/TT_Pythia8_PU20bx25.py
@@ -2,7 +2,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'TTJets_Madgraph_PU20bx25'
+config.General.requestName = 'TT_Pythia8_PU20bx25'
 config.General.workArea = 'workdirCrab'
 
 config.section_("JobType")
@@ -11,13 +11,13 @@ config.JobType.psetName = 'BristolAnalysis/NTupleTools/Configuration/makeTuplesF
 config.JobType.pyCfgParams = ['isTTbarMC=1']
 
 config.section_("Data")
-config.Data.inputDataset = '/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
+config.Data.inputDataset = '/TT_Tune4C_13TeV-pythia8-tauola/Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v1/MINIAODSIM'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 10
 config.Data.outLFN = '/store/user/ejclemen/'
 config.Data.publication = True
-config.Data.publishDataName = 'TTJets_Madgraph_PU20bx25_test'
+config.Data.publishDataName = 'TT_Pythia8_PU20bx25'
 
 config.section_("Site")
 config.Site.storageSite = 'T2_UK_SGrid_Bristol'
