77147d7cc344da3c191c8e9be89c5996760efef1
Emyr Clement <emyr.clement@bristol.ac.uk>
2015-01-12 14:35:29 +0000

    Add new crab configurations.

---
+++ Configuration/crab3Configurations/TTJets_Madgraph_PU30bx50.py
@@ -0,0 +1,24 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = 'TTJets_Madgraph_PU30bx50'
+config.General.workArea = 'workdirCrab/v1/2015-01-09'
+config.General.transferOutputs = True
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'BristolAnalysis/NTupleTools/Configuration/makeTuplesFromMiniAOD_cfg.py'
+config.JobType.pyCfgParams = ['isTTbarMC=1']
+
+config.section_("Data")
+config.Data.inputDataset = '/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Phys14DR-PU30bx50_PHYS14_25_V1-v1/MINIAODSIM'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 10
+config.Data.outLFN = '/store/user/ejclemen/'
+config.Data.publication = True
+config.Data.publishDataName = 'TTJets_Madgraph_PU30bx50'
+
+config.section_("Site")
+config.Site.storageSite = 'T2_UK_SGrid_Bristol'
